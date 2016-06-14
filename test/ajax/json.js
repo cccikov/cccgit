bfe97db9f3f39d4f073d50caaf973b91d1ad9470
ccc <522978139@qq.com>
2016-06-14 21:06:17 +0800

    ajax

---
+++ test/ajax/json.js
@@ -0,0 +1,23 @@
+[
+    {
+        "person": {
+        	"name":"ccc",
+            "position": "前端",
+            "age": "25"
+        }
+    },
+    {
+        "person": {
+        	"name":"杜其坤",
+            "position": "java",
+            "age": "25"
+        }
+    },
+    {
+        "person": {
+        	"name":"唐丽",
+            "position": "前端",
+            "age": "23"
+        }
+    }
+]
---
+++ test/ajax/json2.js
@@ -0,0 +1,302 @@
+var theJson = [
+    {
+        "bigIcon": null,
+        "children": [
+            {
+                "bigIcon": null,
+                "children": [],
+                "code": "role",
+                "fid": "1445659315790S0044I7F000001R5084",
+                "name": "角色管理",
+                "operations": [],
+                "orderNo": 1,
+                "parent": null,
+                "smallIcon": null,
+                "url": "view.system.Role.d"
+            },
+            {
+                "bigIcon": null,
+                "children": [],
+                "code": "user",
+                "fid": "1396189646967S0002I7F000001R5961",
+                "name": "用户管理",
+                "operations": [],
+                "orderNo": 2,
+                "parent": null,
+                "smallIcon": null,
+                "url": "view.system.User.d"
+            },
+            {
+                "bigIcon": "",
+                "children": [],
+                "code": "department",
+                "fid": "1445659202372S0040I7F000001R7456",
+                "name": "部门管理",
+                "operations": [],
+                "orderNo": 3,
+                "parent": null,
+                "smallIcon": null,
+                "url": "view.system.Department.d"
+            },
+            {
+                "bigIcon": null,
+                "children": [],
+                "code": "workflow",
+                "fid": "1447833509841S0127I7F000001R6324",
+                "name": "流程设置",
+                "operations": [],
+                "orderNo": 4,
+                "parent": null,
+                "smallIcon": null,
+                "url": "view.workflow.Workflow.d"
+            },
+            {
+                "bigIcon": null,
+                "children": [],
+                "code": "workflowOrder",
+                "fid": "1447841249528S0124I7F000001R1692",
+                "name": "流程实例",
+                "operations": [],
+                "orderNo": 5,
+                "parent": null,
+                "smallIcon": null,
+                "url": "view.workflow.WorkflowOrder.d"
+            },
+            {
+                "bigIcon": null,
+                "children": [
+                    {
+                        "bigIcon": null,
+                        "children": [],
+                        "code": "role",
+                        "fid": "1445659315790S0044I7F000001R5084",
+                        "name": "角色管理",
+                        "operations": [],
+                        "orderNo": 1,
+                        "parent": null,
+                        "smallIcon": null,
+                        "url": "view.system.Role.d"
+                    }
+                ],
+                "code": "extConfig",
+                "fid": "1447839881348S0006I7F000001R6224",
+                "name": "扩展属性配置",
+                "operations": [],
+                "orderNo": 6,
+                "parent": null,
+                "smallIcon": null,
+                "url": "view.system.ExtConfig.d"
+            },
+            {
+                "bigIcon": null,
+                "children": [{
+                    "bigIcon": null,
+                    "children": [],
+                    "code": "role",
+                    "fid": "1445659315790S0044I7F000001R5084",
+                    "name": "角色管理",
+                    "operations": [],
+                    "orderNo": 1,
+                    "parent": null,
+                    "smallIcon": null,
+                    "url": "view.system.Role.d"
+                },{
+                    "bigIcon": null,
+                    "children": [],
+                    "code": "role",
+                    "fid": "1445659315790S0044I7F000001R5084",
+                    "name": "角色管理",
+                    "operations": [],
+                    "orderNo": 1,
+                    "parent": null,
+                    "smallIcon": null,
+                    "url": "view.system.Role.d"
+                }],
+                "code": "dropdown",
+                "fid": "1447839932896S0009I7F000001R8531",
+                "name": "下拉属性配置",
+                "operations": [],
+                "orderNo": 7,
+                "parent": null,
+                "smallIcon": null,
+                "url": "view.system.Dropdown.d"
+            }
+        ],
+        "code": "system",
+        "fid": "1445995250533S0043I7F000001R9384",
+        "name": "系统管理",
+        "operations": [],
+        "orderNo": 3,
+        "parent": null,
+        "smallIcon": null,
+        "url": null
+    },
+    {
+        "bigIcon": null,
+        "children": [
+            {
+                "bigIcon": null,
+                "children": [],
+                "code": "notice",
+                "fid": "1447063106578S0165I7F000001R5637",
+                "name": "通知公告",
+                "operations": [],
+                "orderNo": 1,
+                "parent": null,
+                "smallIcon": null,
+                "url": "view.system.Notice.d"
+            },
+            {
+                "bigIcon": null,
+                "children": [],
+                "code": "console",
+                "fid": "1447827717581S0349I7F000001R7006",
+                "name": "控制面板",
+                "operations": [],
+                "orderNo": 2,
+                "parent": null,
+                "smallIcon": null,
+                "url": "view.system.UserSetting.d"
+            }
+        ],
+        "code": "person",
+        "fid": "1447213990498S0121I7F000001R0949",
+        "name": "个人事务",
+        "operations": [],
+        "orderNo": 4,
+        "parent": null,
+        "smallIcon": null,
+        "url": null
+    },
+    {
+        "bigIcon": null,
+        "children": [
+            {
+                "bigIcon": null,
+                "children": [{
+                	"bigIcon": null,
+                	"children": [],
+                	"code": "allClient",
+                	"fid": "1448586363316S0030I7F000001R8778",
+                	"name": "所有的客户",
+                	"operations": [],
+                	"orderNo": 2,
+                	"parent": null,
+                	"smallIcon": null,
+                	"url": "view.crm.AllClient.d"
+                },{
+                	"bigIcon": null,
+                	"children": [],
+                	"code": "allClient",
+                	"fid": "1448586363316S0030I7F000001R8778",
+                	"name": "所有的客户",
+                	"operations": [],
+                	"orderNo": 2,
+                	"parent": null,
+                	"smallIcon": null,
+                	"url": "view.crm.AllClient.d"
+                },{
+                	"bigIcon": null,
+                	"children": [],
+                	"code": "allClient",
+                	"fid": "1448586363316S0030I7F000001R8778",
+                	"name": "所有的客户",
+                	"operations": [],
+                	"orderNo": 2,
+                	"parent": null,
+                	"smallIcon": null,
+                	"url": "view.crm.AllClient.d"
+                }],
+                "code": "myClient",
+                "fid": "1448586321205S0025I7F000001R1307",
+                "name": "我的客户",
+                "operations": [],
+                "orderNo": 1,
+                "parent": null,
+                "smallIcon": null,
+                "url": "view.crm.MyClient.d"
+            },
+            {
+                "bigIcon": null,
+                "children": [{
+                	"bigIcon": null,
+                	"children": [],
+                	"code": "allClient",
+                	"fid": "1448586363316S0030I7F000001R8778",
+                	"name": "所有的客户",
+                	"operations": [],
+                	"orderNo": 2,
+                	"parent": null,
+                	"smallIcon": null,
+                	"url": "view.crm.AllClient.d"
+                },{
+                	"bigIcon": null,
+                	"children": [],
+                	"code": "allClient",
+                	"fid": "1448586363316S0030I7F000001R8778",
+                	"name": "所有的客户",
+                	"operations": [],
+                	"orderNo": 2,
+                	"parent": null,
+                	"smallIcon": null,
+                	"url": "view.crm.AllClient.d"
+                },{
+                	"bigIcon": null,
+                	"children": [],
+                	"code": "allClient",
+                	"fid": "1448586363316S0030I7F000001R8778",
+                	"name": "所有的客户",
+                	"operations": [],
+                	"orderNo": 2,
+                	"parent": null,
+                	"smallIcon": null,
+                	"url": "view.crm.AllClient.d"
+                }],
+                "code": "allClient",
+                "fid": "1448586363316S0030I7F000001R8778",
+                "name": "所有的客户",
+                "operations": [],
+                "orderNo": 2,
+                "parent": null,
+                "smallIcon": null,
+                "url": "view.crm.AllClient.d"
+            }
+        ],
+        "code": "client",
+        "fid": "1448586245752S0021I7F000001R4883",
+        "name": "客户管理",
+        "operations": [],
+        "orderNo": 5,
+        "parent": null,
+        "smallIcon": null,
+        "url": null
+    }
+];
+var jsonStr = JSON.stringify(theJson);
+
+var leftJSON = [
+    {"fid": "1448586245752S0021I7F000001R4883","name": "客户管理","url": "view.crm.AllClient.d"},
+    {"fid": "1448586245752S0021I7F000001R4883","name": "客户管理1","url": "view.crm.AllClient.d1"},
+    {"fid": "1448586245752S0021I7F000001R4883","name": "客户管理2","url": "view.crm.AllClient.d2"},
+    {"fid": "1448586245752S0021I7F000001R4883","name": "客户管理3","url": "view.crm.AllClient.d3"},
+    {"fid": "1448586245752S0021I7F000001R4883","name": "客户管理4","url": "view.crm.AllClient.d4"},
+    {"fid": "1448586245752S0021I7F000001R4883","name": "客户管理5","url": "view.crm.AllClient.d5"},
+    {"fid": "1448586245752S0021I7F000001R4883","name": "客户管理6","url": "view.crm.AllClient.d6"},
+    {"fid": "1448586245752S0021I7F000001R4883","name": "客户管理7","url": "view.crm.AllClient.d7"},
+    {"fid": "1448586245752S0021I7F000001R4883","name": "客户管理8","url": "view.crm.AllClient.d8"},
+    {"fid": "1448586245752S0021I7F000001R4883","name": "客户管理9","url": "view.crm.AllClient.d9"},
+    {"fid": "1448586245752S0021I7F000001R4883","name": "客户管10","url": "view.crm.AllClient.d10"},
+    {"fid": "1448586245752S0021I7F000001R4883","name": "客户管11","url": "view.crm.AllClient.d11"},
+    {"fid": "1448586245752S0021I7F000001R4883","name": "客户管12","url": "view.crm.AllClient.d12"},
+    {"fid": "1448586245752S0021I7F000001R4883","name": "客户管13","url": "view.crm.AllClient.d13"},
+    {"fid": "1448586245752S0021I7F000001R4883","name": "客户管14","url": "view.crm.AllClient.d14"},
+    {"fid": "1448586245752S0021I7F000001R4883","name": "客户管15","url": "view.crm.AllClient.d15"},
+    {"fid": "1448586245752S0021I7F000001R4883","name": "客户管16","url": "view.crm.AllClient.d16"},
+    {"fid": "1448586245752S0021I7F000001R4883","name": "客户管17","url": "view.crm.AllClient.d17"},
+    {"fid": "1448586245752S0021I7F000001R4883","name": "客户管18","url": "view.crm.AllClient.d18"},
+    {"fid": "1448586245752S0021I7F000001R4883","name": "客户管19","url": "view.crm.AllClient.d19"},
+    {"fid": "1448586245752S0021I7F000001R4883","name": "客户管20","url": "view.crm.AllClient.d20"},
+    {"fid": "1448586245752S0021I7F000001R4883","name": "客户管21","url": "view.crm.AllClient.d21"},
+    {"fid": "1448586245752S0021I7F000001R4883","name": "客户管22","url": "view.crm.AllClient.d22"}
+];
+var shortcutJsonStr = JSON.stringify(leftJSON);
