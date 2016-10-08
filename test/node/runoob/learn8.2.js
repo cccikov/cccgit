c176da1bfbe6955f1f434bfe8598fe0395e934f8
cccikov <522978139@qq.com>
2016-10-08 09:19:37 +0800

    some

--- test/node/runoob/learn8.2.js
+++ test/node/runoob/learn8.2.js
@@ -1,19 +1,15 @@
+/*EventEmitter 的每个事件由一个事件名和若干个参数组成，事件名是一个字符串，通常表达一定的语义。对于每个事件，EventEmitter 支持 若干个事件监听器。
+当事件触发时，注册到这个事件的事件监听器被依次调用，事件参数作为回调函数参数传递。
+让我们以下面的例子解释这个过程：*/
+
 //event.js文件
 var events = require("events");
+
 var emitter = new events.EventEmitter();
-
-// 监听器 #1
-var listen1 = function(){
-    console.log("监听器 listen1 执行");
-}
-
-// 监听器 #2
-var listen2 = function(){
-    console.log("监听器 listen2 执行");
-}
-
-// 绑定 connection 事件，处理函数为 listener1
-emitter.addListener('connection',listener1);
-
-// 绑定 connection 事件，处理函数为 listener2
-emitter.addListener('connection',listener2);
+emitter.on("someEvent",function(arg1,arg2){
+    console.log("listen1",arg1,arg2);
+});// 绑定一个事件someEvent,注册监听器listen1
+emitter.on("someEvent",function(arg1,arg2){
+    console.log("listen2",arg1,arg2);
+});//someEvent事件 注册监听器listen2
+emitter.emit('someEvent',"参数1","参数2");//触发someEvent事件
