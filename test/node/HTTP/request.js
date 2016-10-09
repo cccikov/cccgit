897bd018f07c6436655f05fadd98b0344f93c8b2
cccikov <522978139@qq.com>
2016-10-09 17:58:52 +0800

    node

---
+++ test/node/HTTP/request.js
@@ -0,0 +1,12 @@
+var http = require('http');
+var querystring = require("querystring");
+
+var postData = querystring.stringify({
+    'content':'一齐期待下一期的课程'
+    'cid':348
+});
+
+// var options = {
+//     hostname
+// }
+
