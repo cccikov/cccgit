d47e169d3bcdb5b9fc7fe83f8fe1095c8633eb7b
ccc <522978139@qq.com>
2020-07-10 14:12:33 +0800

    继承再研究
    Object.create()原型式继承

---
+++ test/javascript面向对象/util.js
@@ -0,0 +1,32 @@
+function show(o) {
+    console.log("\n", "\n", "\n", o, "\n", "\n", "\n", "\n", "\n")
+}
+
+function ranInt(max, min) {
+    if (min > max) {
+        var mid = max;
+        max = min;
+        min = mid;
+    }
+    return parseInt(Math.random() * (max + 1 - min) + min)
+}
+
+
+let color_g = ""
+
+function $$$(str = "<---", color) {
+    if (!color) {
+        let num = ranInt(1, 12) * 30
+        color = "hsl(" + num + ",50%,50%)"
+    }
+    color_g = color;
+    console.log(`%c\n${str}-----------`, `color:${color};font-size:20px;font-weight:700;`)
+}
+
+
+function ___(str = "--->", color) {
+    if (!color) {
+        color = color_g
+    }
+    console.log(`%c-----------${str}\n`, `color:${color};font-size:20px;font-weight:700;`)
+}
